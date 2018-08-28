0e39155206c8c45320864697738d5e6e7e4ba982
Mathew Bishop <39043451+MattyyB@users.noreply.github.com>
2018-08-28 10:52:34 -0500

    random number vars

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,7 @@
 
 // generate target number (random each time)
 // generate crystal values (random each time)
+// assign each crystal a random value each time
 // on click, crystal adds value to current score
 // if current score === target number, add to wins counter, else, add to losses counter
 // restart game without refreshing the page, keeps wins and losses totals
@@ -15,11 +16,18 @@ $(document).ready(function() {
   var wins = $('#wins-text');
   var losses = $('#losses-text');
   var currentScore = $('#current-score');
+
   // crystal buttons
   var blueCrystal = $('#crystal-blue');
   var redCrystal = $('#crystal-red');
   var yellowCrystal = $('#crystal-yellow');
   var whiteCrystal = $('#crystal-white');
+
+  // random number generators
+  var targetRandomNumber = Math.floor(Math.random() * 120) + 19;
+  var crystalRandomNumber = Math.floor(Math.random() *12) + 1;
+  
+    
 
 
 
