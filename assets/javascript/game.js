e5f68b1b495a8484b01f0166a19ca33703bdcbf0
Mathew Bishop <39043451+MattyyB@users.noreply.github.com>
2018-08-28 10:38:50 -0500

    html tweaks, containers, javascript started, global vars declared

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+// target number between 19-120 
+// crystal numbers between 1-12
+
+// generate target number (random each time)
+// generate crystal values (random each time)
+// on click, crystal adds value to current score
+// if current score === target number, add to wins counter, else, add to losses counter
+// restart game without refreshing the page, keeps wins and losses totals
+
+$(document).ready(function() {
+
+    // global var
+    // data containers
+  var targetNumber = $('#target-number');
+  var wins = $('#wins-text');
+  var losses = $('#losses-text');
+  var currentScore = $('#current-score');
+  // crystal buttons
+  var blueCrystal = $('#crystal-blue');
+  var redCrystal = $('#crystal-red');
+  var yellowCrystal = $('#crystal-yellow');
+  var whiteCrystal = $('#crystal-white');
+
+
+
+});
