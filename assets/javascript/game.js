3a0ce2d568d459cae49fdef5451c1057810b11d8
Mathew Bishop <39043451+MattyyB@users.noreply.github.com>
2018-08-30 09:55:43 -0500

    starting over, method of achieving functionality did not match requested method

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,10 @@
 // target number between 19-120 
 // crystal numbers between 1-12
 
-// generate target number (random each time)    √
-// generate crystal values (random each time)   √
-// assign each crystal a random value each time     √
-// on click, crystal adds value to current score  √
+// generate target number (random each time)    
+// generate crystal values (random each time)   
+// assign each crystal a random value each time     
+// on click, crystal adds value to current score  
 // if current score === target number, add to wins counter. if >, add to losses counter    
 // restart game without refreshing the page, keeps wins and losses totals
 
@@ -14,103 +14,11 @@
 
 $(document).ready(function() {
 
-var targetNumber = $('#target-number');
-var wins = 0;
-var losses = 0;
+var targetNumber = Math.floor(Math.random() * 120) + 1;
+
+$('#target-number').text(targetNumber);
+
 var currentScore = 0;
-
-// crystal buttons
-var blueCrystal = $('#crystal-blue');
-var redCrystal = $('#crystal-red');
-var yellowCrystal = $('#crystal-yellow');
-var whiteCrystal = $('#crystal-white');
-
-var targetRandomNumber = Math.floor(Math.random() * 120) + 19;
-    
-function newRound() {
-        var targetRandomNumber = Math.floor(Math.random() * 120) + 19;
-        targetNumber.html(targetRandomNumber);
-        blueCrystal.val(Math.floor(Math.random() *12) + 1);
-        redCrystal.val(Math.floor(Math.random() *12) + 1);
-        yellowCrystal.val(Math.floor(Math.random() *12) + 1);
-        whiteCrystal.val(Math.floor(Math.random() *12) + 1);
-        var currentScore = 0;
-    }
-
-newRound();
-
-blueCrystal.on('click', function() {
-    (currentScore = parseInt(currentScore) + parseInt($(this).val()));
-    $('#current-score').html(currentScore);
-    if (currentScore === targetRandomNumber) {
-        ++wins;
-        alert('You won this round');
-        newRound();
-    }
-    
-    if (currentScore > targetRandomNumber) {
-        ++losses;
-        alert('You lost this round');
-        newRound();
-    }
-    $('#wins-text').html(wins);
-    $('#losses-text').html(losses);
-});
-    
-redCrystal.on('click', function() {
-    (currentScore = parseInt(currentScore) + parseInt($(this).val()));
-    $('#current-score').html(currentScore);
-    if (currentScore === targetRandomNumber) {
-        ++wins;
-        alert('You won this round');
-        newRound();
-    }
-    
-    if (currentScore > targetRandomNumber) {
-        ++losses;
-        alert('You lost this round');
-        newRound();
-    }
-    $('#wins-text').html(wins);
-    $('#losses-text').html(losses);
-});
-
-yellowCrystal.on('click', function() {
-    (currentScore = parseInt(currentScore) + parseInt($(this).val()));
-    $('#current-score').html(currentScore);
-    if (currentScore === targetRandomNumber) {
-        ++wins;
-        alert('You won this round');
-        newRound();
-    }
-    
-    if (currentScore > targetRandomNumber) {
-        ++losses;
-        alert('You lost this round');
-        newRound();
-    }
-    $('#wins-text').html(wins);
-    $('#losses-text').html(losses);
-});
-
-whiteCrystal.on('click', function() {
-    (currentScore = parseInt(currentScore) + parseInt($(this).val()));
-    $('#current-score').html(currentScore);
-    if (currentScore === targetRandomNumber) {
-        ++wins;
-        alert('You won this round');
-        newRound();
-    }
-    
-    if (currentScore > targetRandomNumber) {
-        ++losses;
-        alert('You lost this round');
-        newRound();
-    }
-    $('#wins-text').html(wins);
-    $('#losses-text').html(losses);
-});
-
 
 
 
